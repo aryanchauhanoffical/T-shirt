c4b0c33a71e7ca2044402f9748d41b00a1ab8cc5
Anand <tripletech126@gmail.com>
2024-11-04 15:07:35 +0530

    5th

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 import { easing } from "maath";
-import { useFrame } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { AccumulativeShadows, RandomizedLight } from "@react-three/drei";
 
 const Backdrop = () => {
@@ -12,21 +12,22 @@ const Backdrop = () => {
       temporal
       frames={60}
       alphaTest={0.85}
-      scae={10}
+      scale={5}
+      resolution={2048}
       rotation={[Math.PI / 2, 0, 0]}
       position={[0, 0, -0.14]}
     >
       <RandomizedLight
         amount={4}
         radius={9}
-        intensity={0.55}
+        intensity={0.55 * Math.PI}
         ambient={0.25}
         position={[5, 5, -10]}
       />
       <RandomizedLight
         amount={4}
         radius={5}
-        intensity={0.25}
+        intensity={0.25 * Math.PI}
         ambient={0.55}
         position={[-5, 5, -9]}
       />
